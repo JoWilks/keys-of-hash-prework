27c724831f59271b87046f60b92320d645745661
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-09 12:08:25 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,4 +1,5 @@
 class Hash
+<<<<<<< HEAD
   def keys_of(*arguments)
     array = []
     
@@ -11,6 +12,21 @@ class Hash
           end
         end
             }
+=======
+  def keys_of(arguments)
+    array = []
+    self.each {|keys, values| 
+        values.each {|animal, country| 
+          if country == arguments
+            array << animal
+          elsif
+            animal == arguments
+            array << animal
+          end
+
+            }
+      }
+>>>>>>> 4944427bfd64405094317180c361bfce94d3a193
     array
     end
  end
